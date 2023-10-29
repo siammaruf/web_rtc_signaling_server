e4584f0d05418fcfccab3e0b5bf5e019b0fb928d
Md Siam Maruf <siam.maruf1@gmail.com>
2023-10-29 17:06:32 +0600

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,14 +1,16 @@
 const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
-    origin: (origin, callback)=>{
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-          callback(null, true)
-        } else {
-          callback(new Error('Not allowed by CORS'))
-        }
-      },
-      credentials: false
+    cors:{
+        origin: (origin, callback)=>{
+            if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+              callback(null, true)
+            } else {
+              callback(new Error('Not allowed by CORS'))
+            }
+          },
+          credentials: false
+    }
 }
 
 module.exports = corsOptions
