2a29113bc7541be9b272fce76b6a0b29b3fe0113
Md Siam Maruf <siam.maruf1@gmail.com>
2023-10-29 17:22:24 +0600

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get("/", (req, res)=>{
 const emailToSocketIdMap = new Map();
 const socketIdToEmailMap = new Map();
 
-io.on("connect", (socket)=>{
+io.on("connection", (socket)=>{
     console.log(`Socket Connected`, socket.id);
 
     socket.on("room:join", (data) => {
